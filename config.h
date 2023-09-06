a80598fa1a7edc161394e0c3e080f8974cf78b1d
6367f766 <78221071+6367f766@users.noreply.github.com>
2023-09-06 01:22:37 +0100

    Change terminal to urxvt

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMINAL "st"
-#define TERMCLASS "St"
+#define TERMINAL "urxvt"
+#define TERMCLASS "urxvt"
 #define BROWSER "librewolf"
 
 /* appearance */
